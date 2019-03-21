355dd482bd7883b61cef664ebd61e5249c3f13ff
Kennedy Kariuki <you@example.com>
2019-03-21 19:06:53 +0300

    Dagger setup

---
+++ app/src/main/java/com/techmashinani/filamu/di/AppComponent.kt
@@ -0,0 +1,21 @@
+package com.techmashinani.filamu.di
+
+import android.app.Application
+import com.techmashinani.filamu.FilamuApp
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AppModule::class, AndroidSupportInjectionModule::class])
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance fun application(app: FilamuApp): Builder
+        fun build(): AppComponent
+    }
+
+    fun inject(app: Application)
+}
---
+++ app/src/main/java/com/techmashinani/filamu/di/Injectable.kt
@@ -0,0 +1,4 @@
+package com.techmashinani.filamu.di
+
+interface Injectable {
+}
