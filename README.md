972ed65ab8cdd0a68157b08efe5a1d3a0f1784a0
Kennedy Kariuki <you@example.com>
2019-03-29 16:21:12 +0300

    Added readme meaning of app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-"#Filamu" 
+Filamu - swahili name for movie
+
+Filamu is a movie app created to demonstrate usage of android jetpack
+
+<h3>Dependency Injection</h3>
+Have use <a href="https://google.github.io/dagger/">Dagger</a> for dependency injection
---
+++ app/src/main/java/com/techmashinani/filamu/ui/adapters/viewholders/MovieCategoryHolder.kt
@@ -0,0 +1,9 @@
+package com.techmashinani.filamu.ui.adapters.viewholders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.techmashinani.filamu.model.MovieCategory
+
+class MovieCategoryHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+}
